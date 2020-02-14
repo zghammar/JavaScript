ee1b4f15950f00bfec96aa2158a5f72c939d7787
Zakaria Ghammar <55786119+zghammar@users.noreply.github.com>
2020-02-14 14:22:29 +0100

    commti

--- js/script.js
+++ js/script.js
@@ -3,16 +3,21 @@ var submit = document.getElementById("submit");
 
 
 //Button click
-document.getElementById("submit").onclick = function calcPercentage(){
+submit.onclick = function calcPercentage(){
         let xValue = document.getElementById("xValue").value;
         let yValue = document.getElementById("yValue").value;
         let errorMessage = document.getElementById("error-message");
         let resultOutput = document.getElementById("result");
         if(xValue == "" || yValue == ""){
             errorMessage.innerHTML = "<span>Bitte Werte eingeben</span>";
+            resultOutput.innerHTML = "<span></span>";
         }else{
             let result = xValue * 100 / yValue;
             let returnResult = xValue + " sind " + result + " Prozent von " + yValue;
             resultOutput.innerHTML = "<span>" + returnResult + "</span>";
+            errorMessage.innerHTML = "<span></span>";
         }
 }   
+
+//Notenrechner
+
